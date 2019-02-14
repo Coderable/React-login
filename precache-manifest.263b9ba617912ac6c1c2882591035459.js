73defd02b31fdfeea432be98fae9a8caa4afeff0
Coderable <stanley.hoei@gmail.com>
2019-02-14 16:03:11 +0700

    Updates

---
+++ precache-manifest.263b9ba617912ac6c1c2882591035459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-login/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f2c024293e88f654108a",
+    "url": "/react-login/static/js/runtime~main.f2c02429.js"
+  },
+  {
+    "revision": "e9d5e1a5f2f8b5c8fcd1",
+    "url": "/react-login/static/js/main.e9d5e1a5.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/react-login/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "e9d5e1a5f2f8b5c8fcd1",
+    "url": "/react-login/static/css/main.c15b241a.chunk.css"
+  },
+  {
+    "revision": "dea0808e11933ace816bf013bb3b755e",
+    "url": "/react-login/index.html"
+  }
+];
